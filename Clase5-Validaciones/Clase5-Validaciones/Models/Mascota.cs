661739f0ab81a59ef069ae0e10d9077cbd90a98a
fioreColoca <fiorecoloca99@gmail.com>
2021-05-24 02:43:39 -0300

    Validacion mail y crear una validaicon customer.

--- Clase5-Validaciones/Clase5-Validaciones/Models/Mascota.cs
+++ Clase5-Validaciones/Clase5-Validaciones/Models/Mascota.cs
@@ -6,19 +6,27 @@ using System.Threading.Tasks;
 
 namespace Clase5_Validaciones.Models
 {
-    public class Mascota
+    public class Mascota : ValidationAttribute
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="Campo requerido")]
         public Especie Especie { get; set; }
         [Required(ErrorMessage ="Campo requerido")]
+        [ValidationResult]
         public string Estado { get; set; }
+        [ValidationResult]
         [Range(0, 5000, ErrorMessage = "El peso debe estar entre 0 y 5000")]
         public int? Peso { get; set; }
         public string Color { get; set; }
+        [ValidationResult]
         [StringLength(200, ErrorMessage ="No se puede ingresar mas de 200 caracteres")]
         public string Nombre { get; set; }
+        [ValidationResult]
         public string DatosDeContacto { get; set; }
+        [ValidationResult]
+        [Required(ErrorMessage = "Campo requerido")]
+        [EmailAddress(ErrorMessage = "Ingrese direccion de mail válida")]
+        public string mailContacto { get; set; }
         public List<string> Fotos { get; set; }
 
         public Mascota()
@@ -27,3 +35,5 @@ namespace Clase5_Validaciones.Models
         }
     }
 }
+
+
---
+++ Clase5-Validaciones/Clase5-Validaciones/Models/ValidationResultAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clase5_Validaciones.Models
+{
+    internal class ValidationResultAttribute : ValidationAttribute
+    {
+        public string GetErrorMessage() =>
+        $"Ingrese solo numeros letras y/o simbolos de puntuacion.";
+
+        protected override ValidationResult IsValid(object value,
+        ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var atributo = (string)value;
+                string specialChar = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,";
+
+
+                foreach (var item in specialChar)
+                {
+                    if (atributo.Contains(item))
+                        return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
